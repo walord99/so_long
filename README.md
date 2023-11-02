bbfb6cd59cdc9a1cbce4c56703700b0446c14f31
Walord <benplante99@gmail.com>
2023-11-02 16:10:01 -0400

    fixed animation frames

--- README.md
+++ README.md
@@ -2,4 +2,5 @@
 and thanks for all the fish
 
 look at it go
+
 ![](https://cdn.discordapp.com/attachments/905657406623731732/1169721254626984118/satyr.gif)
--- src/loop_hook.c
+++ src/loop_hook.c
@@ -6,20 +6,20 @@
 /*   By: bplante <bplante@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/29 17:15:54 by bplante           #+#    #+#             */
-/*   Updated: 2023/11/02 15:02:44 by bplante          ###   ########.fr       */
+/*   Updated: 2023/11/02 15:49:38 by bplante          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "so_long.h"
 
-void animate(t_game *game, t_list *coll_list)
+void	animate(t_game *game, t_list *coll_list)
 {
-	t_collectible *coll;
+	t_collectible	*coll;
 
 	game->animate_delay += game->mlx->delta_time;
-	if(game->animate_delay < 0.2)
-		return;
-	while(coll_list)
+	if (game->animate_delay < 0.2)
+		return ;
+	while (coll_list)
 	{
 		coll_list = coll_list->next;
 	}
@@ -27,7 +27,6 @@ void animate(t_game *game, t_list *coll_list)
 	game->player.current_frame = (game->player.current_frame + 1) % 6;
 	game->images->player_frames[game->player.current_frame]->enabled = true;
 	game->animate_delay = 0;
-
 }
 
 void	loop_hook(void *param)
@@ -35,7 +34,6 @@ void	loop_hook(void *param)
 	t_game	*game;
 
 	game = (t_game *)param;
-	if(game->game_state == PLAYING)
+	if (game->game_state == PLAYING)
 		animate(game, game->collectables);
-    
 }
